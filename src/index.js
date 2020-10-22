4dc07106800f0d5cc44f5f39d9a8881a9542c7b6
Jorge Rivera <jorge.rivera@ssde.com.mx>
2020-10-22 01:06:01 -0500

    Fix typo rendering the template
    ReactDOM variable was not called appropriately
    Changed  tasks to make more sense
    Added sass loader
    Added scss style to change look and feel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/style.scss'
 
-reactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
@@ -18,8 +18,16 @@ module.exports = {
                         presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
     },
     plugins: [
         new HtmlWebpackPlugin({
